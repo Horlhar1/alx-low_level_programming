a0be402c849d0378a2dbf3c8efc77064b32ae55c
Horlhar1 <horlhardev@gmail.com>
2022-10-16 12:22:48 -0700

    base10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Printing base 10 numbers
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int base10;
+
+		for (base10 = 0; base10 < 10; base10++)
+			printf("%d", base10);
+
+		printf("\n");
+
+		return (0);
+}
