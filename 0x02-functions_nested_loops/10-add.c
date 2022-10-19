33dced50969cc8ee6609adcf92b07b923b042652
Horlhar1 <horlhardev@gmail.com>
2022-10-19 16:53:19 -0700

    Addition of numbers 10-add.cwq

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result
+ * @seven: first number input
+ * @twenty: second number input
+ * Return: one + two
+ */
+int add(int seven, int twenty)
+{
+	return (seven + twenty);
+}
