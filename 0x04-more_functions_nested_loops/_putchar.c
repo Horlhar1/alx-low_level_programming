ecae0e940ef4316c69f24409f9b9757ea35320d8
Horlhar1 <horlhardev@gmail.com>
2022-10-21 19:23:36 -0700

    printing of 0-9

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * _putchar - defining function and prorotypes
+ * Return: putchar(c)
+ * @c: character to be used
+ */
+
 int _putchar(char c)
 {
 	return (putchar(c));
