0e69a145107e8664cdb2d415117eebb6edd41092
Horlhar1 <horlhardev@gmail.com>
2022-10-25 02:25:23 +0000

    task 1 pointers and arrays, resetting an int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - function that resets an int to 98 from its value
+  *			using a pointer.
+  * Return: 0 (success)
+  * @n: the integer to be resetted
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - defining a function (_putchar) - putchar
+ * Return: returning _putchar as putchar
+ */
+
+int _putchar(char c)
+{
+	return (putchar(c));
+}
+
