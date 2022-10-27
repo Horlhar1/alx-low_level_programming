d1613f7d86950a9c9f4795621bde5b9f40bb8783
Horlhar1 <horlhardev@gmail.com>
2022-10-27 16:12:02 +0000

    concatenating two strings again??

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *_strncat - func that concatenates two strings together
+ *@src: parameter to append to dest
+ *@dest: parameter to be appended to
+ *@n: maximum number bytes usable
+ *Return: 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] += src[j];
+		i++;
+		j++;
+	}
+
+	dest += '\0';
+
+	return (dest);
+}
