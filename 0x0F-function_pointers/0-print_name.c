4aba8c9d751c322a0dfe9d5f296bc2e874158b9a
Horlhar1 <horlhardev@gmail.com>
2022-11-17 21:07:24 +0000

    a function that prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+*print_name - print name if have functions
+*@name: string
+*@f: void function(string)
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+		f(name);
+}
---
+++ 0x0F-function_pointers/_putchar.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+int _putchar(char c)
+{
+        return (putchar(c));
+}
